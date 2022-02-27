b458ecd8beb0a70adfb024729beb7763a4ab5886
Harry Buxton <92524519+hbux@users.noreply.github.com>
2022-02-27 12:35:39 +0000

    Add WPF project with MVVM
    Adds a new project for WPF user interface and adds Caliburn.Micro package which also contains Simple Container for dependency injection.

---
+++ TRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,63 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        // Using Caliburn's built-in dependency injection rather than adding another package
+        // for DI
+        private SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            // Looking in the ViewModels folder and gives the implemention of the type
+            // e.g. If ShellViewModel is requested, ShellViewModel is returned
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // Runs this ViewMdodel on start-up as the base view
+            // This is similar to right-clicking a project and 'set project as start-up'
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
