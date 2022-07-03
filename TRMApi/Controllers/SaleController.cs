2904c0bab25a6746a20cc45e5b677dc68b49458c
Harry Buxton <92524519+hbux@users.noreply.github.com>
2022-07-03 13:31:37 +0100

    Updates controllers for API

--- TRMApi/Controllers/SaleController.cs
+++ TRMApi/Controllers/SaleController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Security.Claims;
 using TRMDataManager.Library.DataAccess;
@@ -13,10 +14,17 @@ namespace TRMApi.Controllers
     [ApiController]
     public class SaleController : ControllerBase
     {
+        private IConfiguration _config;
+
+        public SaleController(IConfiguration config)
+        {
+            _config = config;
+        }
+
         [Authorize(Roles = "Cashier")]
         public void Post(SaleModel sale)
         {
-            SaleData data = new SaleData();
+            SaleData data = new SaleData(_config);
             string cashierId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
             data.SaveSale(sale, cashierId);
@@ -26,7 +34,7 @@ namespace TRMApi.Controllers
         [Route("GetSalesReport")]
         public List<SaleReportModel> GetSalesReport()
         {
-            SaleData data = new SaleData();
+            SaleData data = new SaleData(_config);
 
             return data.GetSaleReport();
         }
