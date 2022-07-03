e3aefc018447c849a28c656fd3f27a2472c0e803
Harry Buxton <92524519+hbux@users.noreply.github.com>
2022-07-03 12:58:35 +0100

    Add new api  project and functionality

---
+++ TRMApi/Controllers/SaleController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class SaleController : ControllerBase
+    {
+        [Authorize(Roles = "Cashier")]
+        public void Post(SaleModel sale)
+        {
+            SaleData data = new SaleData();
+            string cashierId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            data.SaveSale(sale, cashierId);
+        }
+
+        [Authorize(Roles = "Admin,Manager")]
+        [Route("GetSalesReport")]
+        public List<SaleReportModel> GetSalesReport()
+        {
+            SaleData data = new SaleData();
+
+            return data.GetSaleReport();
+        }
+    }
+}
---
+++ TRMApi/Controllers/UserController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TRMApi.Data;
+using TRMApi.Models;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private UserManager<IdentityUser> _userManager;
+
+        public UserController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public UserModel GetById()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            UserData data = new UserData();
+
+            return data.GetUserById(userId).First();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        [Route("Admin/GetAllUsers")]
+        public List<ApplicationUserModel> GetAllUsers()
+        {
+            List<ApplicationUserModel> allUsers = new List<ApplicationUserModel>();
+
+            var users = _context.Users.ToList();
+            var userRoles = from userRole in _context.UserRoles
+                            join role in _context.Roles
+                            on userRole.RoleId equals role.Id
+                            select new { userRole.UserId, userRole.RoleId, role.Name };
+
+            foreach (var user in users)
+            {
+                ApplicationUserModel u = new ApplicationUserModel()
+                {
+                    Id = user.Id,
+                    Email = user.Email
+                };
+
+                u.Roles = userRoles.Where(x => x.UserId == u.Id).ToDictionary(key => key.RoleId, value => value.Name);
+
+                allUsers.Add(u);
+            }
+
+            return allUsers;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        [Route("Admin/GetAllRoles")]
+        public Dictionary<string, string> GetAllRoles()
+        {
+            return _context.Roles.ToDictionary(x => x.Id, x => x.Name);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        [Route("Admin/AddUserToRole")]
+        public async Task AddUserToRole(UserRolePairModel pairing)
+        {
+            var user = await _userManager.FindByIdAsync(pairing.UserId);
+            await _userManager.AddToRoleAsync(user, pairing.Role);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        [Route("Admin/RemoveUserFromRole")]
+        public async Task RemoveUserFromRole(UserRolePairModel pairing)
+        {
+            var user = await _userManager.FindByIdAsync(pairing.UserId);
+            await _userManager.RemoveFromRoleAsync(user, pairing.Role);
+        }
+    }
+}
